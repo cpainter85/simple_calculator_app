4796b18910feb1a4cb27bb0aaaa1f59acbbbf4a8
Charles Painter <cpainter85@gmail.com>
2015-07-07 14:37:46 -0400

    can perform arithmetic

--- calculator.js
+++ calculator.js
@@ -1,8 +1,44 @@
 var calculator = {
   addition: function (string) {
-    return string.split('+').reduce(function (sum, current) {
-      return sum+Number(current);
-    },0)
+    return string.split('+').reduce(function (prev, current) {
+      return Number(prev) + Number(current);
+    })
+  },
+  subtraction: function (string) {
+    return string.split('-').reduce(function (prev, current) {
+      return Number(prev) - Number(current);
+    })
+  },
+  multiplication: function (string) {
+    return string.split('*').reduce(function (prev, current) {
+      return Number(prev) * Number(current);
+    })
+  },
+  division: function (string) {
+    return string.split('/').reduce(function (prev, current) {
+      return Number(prev) / Number(current);
+    })
+  },
+  arithmetic: function (string) {
+    var subString = string.split('-');
+
+    subString.forEach(function (subPart, index) {
+      var addString = subPart.split('+');
+
+      addString.forEach(function (addPart, i) {
+        var multString = addPart.split('*');
+
+        multString.forEach(function (multPart, j) {
+          multString[j] = this.division(multPart);
+        }.bind(calculator));
+
+        addString[i] = this.multiplication(multString.join('*'))
+      }.bind(calculator));
+
+      subString[index] = this.addition(addString.join('+'))
+    }.bind(calculator));
+
+    return this.subtraction(subString.join('-'));
   }
 }
 
--- spec/spec.js
+++ spec/spec.js
@@ -5,3 +5,27 @@ describe('#addition', function () {
     expect(calculator.addition('5+7+9')).toEqual(21);
   });
 })
+
+describe('#subtraction', function () {
+  it ('returns the result of subtracting', function () {
+    expect(calculator.subtraction('88-4-12')).toEqual(72);
+  });
+})
+
+describe('#multiplication', function () {
+  it ('returns the product of multiplying the numbers in a given string', function () {
+    expect(calculator.multiplication('14*9*2')).toEqual(252);
+  });
+})
+
+describe('#division', function () {
+  it ('returns the quotient after dividing numbers in a string', function () {
+    expect(calculator.division('44/11')).toEqual(4);
+  });
+})
+
+describe('#arithmetic', function () {
+  it('returns the result of performing the arithmetic operations in a given string', function () {
+    expect(calculator.arithmetic('60+6/3-7*3')).toEqual(41);
+  });
+})
